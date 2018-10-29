71718595c7059c3fe47ba7453b432f5898c6c58c
Sviatoslav <qqfittoppt@gmail.com>
2018-10-29 21:43:59 +0200

    Level 1 Completed

---
+++ WebApi/Controllers/BookController.cs
@@ -0,0 +1,117 @@
+﻿// <copyright file="BookController.cs" company="My company">
+//     Copyright (c) My company". All rights reserved.
+// </copyright>
+
+namespace WebApi.Controllers
+{
+    using System.Collections.Generic;
+    using Microsoft.AspNetCore.Mvc;
+    using WebApi.BookService;
+    using WebApi.Models;
+
+    /// <summary>
+    /// Book controller
+    /// </summary>
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BookController : ControllerBase
+    {
+        /// <summary>
+        /// IBook interface link
+        /// </summary>
+        private IBook books;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="BookController"/> class
+        /// </summary>
+        /// <param name="books">IBook interface link</param>
+        public BookController(IBook books)
+        {
+            this.books = books;
+        }
+
+        /// <summary>
+        /// Getting all books method
+        /// </summary>
+        /// <returns>List of books</returns>
+        [HttpGet]
+        public ActionResult<IEnumerable<Book>> GetAllBooks()
+        {
+            List<Book> booksList = this.books.GetAllBooks();
+            if (booksList.Count != 0)
+            {
+                return this.Ok(booksList);
+            }
+            else
+            {
+                return this.NotFound();
+            }
+        }
+
+        /// <summary>
+        /// Getting single book by id
+        /// </summary>
+        /// <param name="id">Book id to get</param>
+        /// <returns>Book or NotFound</returns>
+        [HttpGet("{id}")]
+        public IActionResult GetBookById(int id)
+        {
+            Book resultBook = this.books.GetBookById(id);
+            if (resultBook == null)
+            {
+                return this.NotFound();
+            }
+
+            return this.Ok(resultBook);
+        }
+        
+        /// <summary>
+        /// Adding book
+        /// </summary>
+        /// <param name="book">Book to add</param>
+        /// <returns>Added book</returns>
+        [HttpPost]
+        public IActionResult AddBook([FromBody] Book book)
+        {
+            this.books.AddBook(book);
+            return this.Created("books", book);
+        }
+
+        /// <summary>
+        /// Updating book by id
+        /// </summary>
+        /// <param name="id">Id of book to update</param>
+        /// <param name="book">Data for update</param>
+        /// <returns>Book or NotFound</returns>
+        [HttpPut("{id}")]
+        public IActionResult UpdateBook(int id, [FromBody] Book book)
+        {
+            if (this.books.UpdateBook(id, book))
+            {
+                return this.Ok(book);
+            }
+            else
+            {
+                return this.NotFound();
+            }
+        }
+
+        /// <summary>
+        /// Deleting book by id
+        /// </summary>
+        /// <param name="id">Id of book</param>
+        /// <returns>Ok or BadRequest</returns>
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            if (this.books.RemoveBook(id))
+            {
+                return this.Ok();
+            }
+            else
+            {
+                return this.BadRequest("There is no book with such id!");
+            }
+        }
+    }
+}
---
+++ WebApi/Models/Book.cs
@@ -0,0 +1,53 @@
+﻿// <copyright file="Book.cs" company="My company">
+//     Copyright (c) My company". All rights reserved.
+// </copyright>
+
+namespace WebApi.Models
+{
+    /// <summary>
+    /// Model of book for UI and Back
+    /// </summary>
+    public class Book
+    {
+        /// <summary>
+        /// Static counter for unique id creation
+        /// </summary>
+        private static int idCount;
+
+        /// <summary>
+        /// Initializes static members of the <see cref="Book"/> class
+        /// </summary>
+        static Book()
+        {
+            idCount = 0;
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Book"/> class
+        /// </summary>
+        /// <param name="title">Title of book</param>
+        public Book(string title)
+        {
+            this.Id = idCount++;
+            this.Title = title;
+        }
+
+        /// <summary>
+        /// Gets Id 
+        /// </summary>
+        public int Id
+        {
+            get;
+            private set;
+        }
+
+        /// <summary>
+        /// Gets or sets title of book
+        /// </summary>
+        public string Title
+        {
+            get;
+            set;
+        }
+    }
+}
